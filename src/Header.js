4c1cc1a30e8dbbea20acff111c9f3ee2ab4d299a
Yogesh007587 <yogeshtripathi2002@gmail.com>
2023-04-15 11:23:46 +0530

    hh

---
+++ src/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Header.css'
+
+export default function Header() {
+    return (
+        <div>
+            <ul class="navbar">
+                <li class="nav-link">
+                    <a href="#">About</a>
+                </li>
+                <li class="nav-link">
+                    <a href="#">Work</a>
+                </li>
+                <li class="nav-link">
+                    <a href="#">Contact</a>
+                </li>
+            </ul>
+
+        </div>
+    )
+}
---
+++ src/MainContent.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Main_content() {
+    return (
+        <div>Main_content</div>
+    )
+}
